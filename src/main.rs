96dce33b761f3a971ab4df09d655c49b2ea2d3a4
Exr0n <mail@exr0n.com>
2021-06-23 11:02:04 -0700

    mvp-v0 (shorter adjective list)

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,18 @@
-use std::{ env, fs, io };
-
-use std::io::BufRead;
-use std::fs::File;
-use std::path::Path;
+use std::{ env };
 
 use rand::prelude::{ thread_rng, IteratorRandom };
 
-//fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-//where P: AsRef<Path>, {
-//    // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
-//    let file = File::open(filename)?;
-//    Ok(io::BufReader::new(file).lines())
-//}
-
 fn main() {
-    //let path_intensifiers = read_lines("intensifiers.txt").expect("Error loading intensifiers!");
-    //let path_adjectives   = read_lines("adjectives.txt")  .expect("Error loading adjectives!");
-    //let path_nouns        = read_lines("nouns.txt")       .expect("Error loading nouns!");
-
     let path_intensifiers = String::from_utf8_lossy(include_bytes!("intensifiers.txt"));
     let path_adjectives   = String::from_utf8_lossy(include_bytes!("adjectives.txt")  );
     let path_nouns        = String::from_utf8_lossy(include_bytes!("nouns.txt")       );
 
     let mut rng = thread_rng();
-    println!("random intensifier: {}", path_intensifiers.lines().choose(&mut rng).unwrap());
+    let     sep = '-';
+    println!("{}{}{}{1}{}",
+             path_intensifiers.lines().choose(&mut rng).unwrap(),
+             sep,
+             path_adjectives  .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap()
+        );
 }
