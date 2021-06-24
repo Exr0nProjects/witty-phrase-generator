dbcc3f727ef9d1457b25e9e27c3d386d3b572fc1
Exr0n <mail@exr0n.com>
2021-06-23 22:49:50 -0700

    some basic cli args

--- readme.md
+++ readme.md
@@ -2,5 +2,13 @@
 
 Generates three-word phrases of the form `intensifier-adjective-noun`, just like GitHub default repo names.
 
-Has minimal dependencies (just `rand`) and minimal bloat. 
+Has minimal dependencies (just `rand` and `getopts`) and minimal bloat. 
+
+## Usage
+
+```
+$ witty-phrase-generator        # -> staggeringly-wise-alchohol
+$ witty-phrase-generator -2     # -> fantastic brush
+$ witty-phrase-generator -n 4   # (outputs 4 lines of 3-word phrases)
+```
 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
 use rand::prelude::{ thread_rng, IteratorRandom };
 
 fn main() {
-    let path_intensifiers = String::from_utf8_lossy(include_bytes!("intensifiers.txt"));
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optopt("n", "num", "set number of phrases to generate", "NUM");
+    opts.optopt("s", "sep", "set the character used between words, default is -", "CHAR");
+    opts.optflag("2", "", "generate a two word phrase (adjective-noun)");
+    opts.optflag("h", "help", "print this help menu then exit");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!("{}", f.to_string())
+    };
+
+    if matches.opt_present("h") {
+        println!("{}", opts.usage("Usage: witty-phrase-generator [options]"));
+        return;
+    }
+
+    let mut rng = thread_rng();
+    let     sep = matches.opt_get_default("s", "-".to_string())
+                         .expect("Could not parse separator!");
+
+    if !matches.opt_present("2") {
+        let path_intensifiers = String::from_utf8_lossy(include_bytes!("intensifiers.txt"));
+        print!("{}{}", path_intensifiers.lines().choose(&mut rng).unwrap(), sep);
+    };
+
     let path_adjectives   = String::from_utf8_lossy(include_bytes!("adjectives.txt")  );
     let path_nouns        = String::from_utf8_lossy(include_bytes!("nouns.txt")       );
 
-    let mut rng = thread_rng();
-    let     sep = '-';
-    println!("{}{}{}{1}{}",
-             path_intensifiers.lines().choose(&mut rng).unwrap(),
-             sep,
+    println!("{}{}{}{}{}{}{}{}{}{}{}",
              path_adjectives  .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap()
+             sep,
+             path_nouns       .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap(),
+             path_nouns       .lines().choose(&mut rng).unwrap(),
         );
 }
