003725bfc7eae663c5cbbdf5fd2422c2f4439aa3
Exr0n <mail@exr0n.com>
2021-06-23 23:01:03 -0700

    need to restructure to support alliteration

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     opts.optopt("n", "num", "set number of phrases to generate", "NUM");
     opts.optopt("s", "sep", "set the character used between words, default is -", "CHAR");
     opts.optflag("2", "", "generate a two word phrase (adjective-noun)");
+    opts.optflag("a", "alliterate", "force words in a phrase to start with the same letter");
     opts.optflag("h", "help", "print this help menu then exit");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
@@ -20,29 +21,25 @@ fn main() {
         return;
     }
 
-    let mut rng = thread_rng();
-    let     sep = matches.opt_get_default("s", "-".to_string())
-                         .expect("Could not parse separator!");
-
-    if !matches.opt_present("2") {
-        let path_intensifiers = String::from_utf8_lossy(include_bytes!("intensifiers.txt"));
-        print!("{}{}", path_intensifiers.lines().choose(&mut rng).unwrap(), sep);
-    };
-
+    let path_intensifiers = String::from_utf8_lossy(include_bytes!("intensifiers.txt"));
     let path_adjectives   = String::from_utf8_lossy(include_bytes!("adjectives.txt")  );
     let path_nouns        = String::from_utf8_lossy(include_bytes!("nouns.txt")       );
 
-    println!("{}{}{}{}{}{}{}{}{}{}{}",
-             path_adjectives  .lines().choose(&mut rng).unwrap(),
-             sep,
-             path_nouns       .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap(),
-             path_nouns       .lines().choose(&mut rng).unwrap(),
+    let mut rng = thread_rng();
+    let     num = matches.opt_get_default("n", 1)
+                         .expect("Could not parse line count!");
+    let     sep = matches.opt_get_default("s", "-".to_string())
+                         .expect("Could not parse separator!");
+
+    for _ in 0..num {
+        if !matches.opt_present("2") {
+            print!("{}{}", path_intensifiers.lines().choose(&mut rng).unwrap(), sep);
+        };
+
+        println!("{}{}{}",
+            path_adjectives  .lines().choose(&mut rng).unwrap(),
+            sep,
+            path_nouns       .lines().choose(&mut rng).unwrap(),
         );
+    }
 }
